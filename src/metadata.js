1f1169287394a7d481f61155aa96d052083f7f03
Peak Funnels <support@peakfunnels.co.za>
2025-05-20 01:54:44 +0200

    fix: export showMetadata and use cp for CSS copy

---
+++ src/metadata.js
@@ -0,0 +1,9 @@
+// src/metadata.js
+
+export async function showMetadata(fileEntry, modalContainer) {
+  // TODO: extract EXIF or video metadata, then call renderMetaModal
+}
+
+export function renderMetaModal(data, modalElement) {
+  // TODO: build table in modalElement from data object
+}
