bd04cb9b938f9af7ae4046e33119992f82590fce
Peak Funnels <support@peakfunnels.co.za>
2025-05-20 15:00:35 +0200

    chore: restore original single-file widget

--- src/utils.js
+++
@@ -1,4 +0,0 @@
-// src/utils.js
-export const IMAGE_EXTS = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.tiff'];
-export const VIDEO_EXTS = ['.mp4', '.mov', '.webm'];
-export const BATCH_SIZE = 30;
